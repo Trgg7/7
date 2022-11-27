2b33b835aa7a9211b09c1739f69d0cdb9726c41b
Trgg7 <118647690+Trgg7@users.noreply.github.com>
2022-11-26 21:33:11 -0800

    Update task1.sv

--- task1.sv
+++ task1.sv
@@ -6,6 +6,45 @@ wire [7:0] next_pc;
 wire [7:0] pc_add;
 wire [7:0] keep_pc;
 
+
+
+reg instr,
+reg_sel, 
+opcode, 
+ALU_op, 
+shift_op, 
+sximm5,  
+sximm8, 
+r_addr, 
+w_addr
+clk, 
+ram_r_data, 
+keep_pc, 
+wb_sel, 
+w_addr, 
+w_en, 
+r_addr, 
+en_A,
+en_B, 
+shift_op, 
+sel_A,
+sel_B, 
+ALU_op, 
+en_C, 
+en_status,
+sximm8, 
+sximm5,
+datapath_out, 
+Z1, 
+N1, 
+V1
+
+start,waiting,reg_sel, wb_sel, w_en,en_A, en_B, en_C, en_status,sel_A, sel_B, clear_pc, load_pc, load_addr, ram_w_addr, sel_addr, load_ir
+clk, ram_w_en, ram_r_addr, ram_w_addr, datapath_out, ram_r_data
+
+
+
+
 always @(*) begin
 pc_add = keep_pc + 1'b1
 
@@ -27,8 +66,6 @@ instr <= (load_ir) ram_r_data : instr;
 
 data_address <= (load_addr) ? datapath_out[7:0] : data_address;
 
-
-
 end
 
 
@@ -38,3 +75,5 @@ controller U02(clk, rst_n, start,opcode, ALU_op, shift_op,Z1, N1, V1,waiting,reg
 ram U03(clk, ram_w_en, ram_r_addr, ram_w_addr, datapath_out, ram_r_data);
 
 endmodule: task1
+
+
