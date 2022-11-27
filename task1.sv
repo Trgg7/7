7f4de012584b933fa41e6ecfd22886ff403ff9c0
Trgg7 <118647690+Trgg7@users.noreply.github.com>
2022-11-26 22:03:49 -0800

    Update task1.sv

--- task1.sv
+++ task1.sv
@@ -8,39 +8,49 @@ wire [7:0] keep_pc;
 
 
 
-reg instr,
-reg_sel, 
-opcode, 
-ALU_op, 
-shift_op, 
-sximm5,  
-sximm8, 
-r_addr, 
-w_addr
-clk, 
-ram_r_data, 
-keep_pc, 
-wb_sel, 
-w_addr, 
-w_en, 
-r_addr, 
-en_A,
-en_B, 
-shift_op, 
-sel_A,
-sel_B, 
-ALU_op, 
-en_C, 
-en_status,
-sximm8, 
-sximm5,
-datapath_out, 
-Z1, 
-N1, 
-V1
+reg instr;
+reg reg_sel;
+wire [2:0] opcode;
+wire[1:0] ALU_op;
+wire [1:0] shift_op;
+wire[15:0] sximm5;
+wire[15:0] sximm8;
+wire[2:0] r_addr;
+wire[2:0] w_addr;
+wire [7:0] ram_r_data;
+wire keep_pc;
+reg [1:0] wb_sel;
 
-start,waiting,reg_sel, wb_sel, w_en,en_A, en_B, en_C, en_status,sel_A, sel_B, clear_pc, load_pc, load_addr, ram_w_addr, sel_addr, load_ir
-clk, ram_w_en, ram_r_addr, ram_w_addr, datapath_out, ram_r_data
+reg w_en;
+reg en_A;
+reg en_B;
+reg en_C;
+reg en_status;
+reg sel_A;
+reg sel_B;
+
+
+reg w_addr; 
+reg w_en;
+wire r_addr;
+wire datapath_out;
+wire Z1;
+wire N1;
+wire V1;
+
+wire start;
+wire waiting;
+wire clear_pc;
+wire load_pc;
+wire load_addr;
+wire ram_w_addr;
+wire sel_addr;
+wire load_ir;
+wire ram_w_en;
+wire ram_r_addr;
+wire ram_w_addr;
+wire datapath_out;
+wire ram_r_data;
 
 
 
@@ -52,7 +62,6 @@ next_pc= (clear_pc) ? start_pc : pc_add;
 
 ram_r_addr = (sel_addr)? keep_pc : data_address;
 ram_w_addr = (sel_addr)? keep_pc : data_address;
-
 
 end 
 
